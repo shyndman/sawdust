c5cc19bed15ea8e848021411a61a2171a01e63bf
Scott Hyndman <scotty.hyndman@gmail.com>
2019-09-07 16:56:14 -0400

    Split into two packages, core and codegen

---
+++ sawdust_codegen/example/sawdust_codegen_example.dart
@@ -0,0 +1,6 @@
+import 'package:sawdust_codegen/sawdust_codegen.dart';
+
+main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
--- sawdust_codegen/lib/sawdust_codegen.dart
+++ sawdust_codegen/lib/sawdust_codegen.dart
@@ -1,8 +1,8 @@
 /// Support for doing something awesome.
 ///
 /// More dartdocs go here.
-library sawdust;
+library sawdust_codegen;
 
-export 'src/sawdust_base.dart';
+export 'src/sawdust_codegen_base.dart';
 
 // TODO: Export any libraries intended for clients of this package.
